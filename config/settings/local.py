70a187ff425efc12d628d33fe7636219e71541bd
Amir Hitavi <a.hitavi@yahoo.com>
2024-09-29 13:15:46 +0330

    first commit: start the project and config the django project

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+from .base import * # noqa
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-yp8qy+04_^8!vbhv7i9k6db!7inw!x%udvnic2liueoo03_5zv",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
